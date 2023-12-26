9b7654456857a6c6ccd43b3f8199c5d3f9c095d2
kirtiman12 <kirtiman49@gmail.com>
2023-12-26 17:16:21 +0530

    changes

--- frontend/src/components/miscellaneous/SideDrawer.js
+++ frontend/src/components/miscellaneous/SideDrawer.js
@@ -1,9 +1,61 @@
-import React from "react";
+import { Box, Button, Tooltip, Text, Menu, MenuButton, Avatar, MenuList, MenuItem } from "@chakra-ui/react";
+import {BellIcon, ChevronDownIcon} from "@chakra-ui/icons";
+import React, { useState } from "react";
+import { ChatState } from "../../Context/ChatProvider";
 
-const SideDrawer = () =>{
-    return <div>
-        SideDrawer
-    </div>
-}
+
+const SideDrawer = () => {
+  const {user} = ChatState()
+  const [search, setSearch] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
+  const [loading, setloading] = useState(false);
+  const [loadingChat, setloadingChat] = useState();
+
+  
+  return (
+    <Box
+      display="flex"
+      justifyContent="space-between"
+      alignItems="center"
+      backgroundColor="white"
+      width="100%"
+      padding="5px 10px"
+      borderWidth="5px"
+    >
+      <Tooltip label="Search Users to chat" hasArrow placement="bottom-end">
+        <Button variant="ghost">
+          <i className="fa-brands fa-searchengin"></i>
+          <Text display={{ base: "none", md: "flex" }} paddingLeft={4}>
+            Search User
+          </Text>
+        </Button>
+      </Tooltip>
+
+      <Text fontSize="2xl" fontFamily="Work Sans Pro" flex="1" textAlign="center">
+        Chatter-Patter
+      </Text>
+
+      <Menu>
+        <MenuButton padding={1}>
+          <BellIcon fontSize="2xl" margin={1} />
+        </MenuButton>
+        {/* <MenuList></MenuList> */}
+      </Menu>
+      <Menu>
+      <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+          <Avatar 
+          size="sm"
+          cursor="pointer"
+          name={user.name}
+          src={user.pic}/>
+        </MenuButton>
+        <MenuList>
+            <MenuItem>My Profile</MenuItem>
+            <MenuItem>Logout</MenuItem>
+        </MenuList>
+      </Menu>
+    </Box>
+  );
+};
 
 export default SideDrawer;
